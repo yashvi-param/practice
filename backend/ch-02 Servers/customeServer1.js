7f85a1f34fea3b0a98ee1389e71327b7f7bd3e6d
yashvi-param <yashvirwn123@gmail.com>
2026-03-26 13:31:47 +0530

    Add files via upload
    complete

--- backend/ch-02 Servers/customeServer1.js
+++ backend/ch-02 Servers/customeServer1.js
@@ -1,25 +1,12 @@
-// import http from "http";
+import http from "http";
 
-// const server = http.createServer((req,res) =>{
-//     res.writeHead(200, {"content-type":"text/html"});
-//     res.end("<h1>hello from node server</h1>")
-// });
+const server = http.createServer((req,res)=>{
+    res.writeHead(200, {"content-type":"text/html"});
+    res.end("<h1>hello from node server</h1>")
+})
 
-// const port = 5001;
+const port = 5001;
 
-// server.listen(port, ()=>{
-//     console.log("server running on port", port)
-// });
-
-// import http from "http";
-
-// const server = http.createServer((req,res)=>{
-//     res.writeHead(200, {"content-type":"text/html"});
-//     res.end("<h1>hello from node server</h1>")
-// })
-
-// const port = 5001;
-
-// server.listen(port, ()=>{
-//     console.log("server running on port", port)
-// })
+server.listen(port, ()=>{
+    console.log("server running on port", port)
+})
--- backend/ch-02 Servers/fileServer.js
+++ backend/ch-02 Servers/fileServer.js
@@ -1,51 +1,26 @@
-// import http from "http";
-// import fs from "fs";
 
-// const server = http.createServer((req, res) => {
-//   fs.readFile("index.html", (err, data) => {
-//     if (err) {
-//       res.writeHead(404);
-//       res.end("file not found");
-//     } else {
-//       res.writeHead(200, { "content-type": "text/html" });
-//       res.end(data);
-//     }
-//   });
-// });
+import http from "http";
+import fs from "fs";
 
-// const port = 5000;
+const server = http.createServer((req,res) =>{
+    fs.readFile("index.html", (err,data) =>{
+        if (err){
+            res.writeHead(404)
+            res.end("file not found");
+        }
+        else{
+            res.writeHead(200, {"content-type":"text/html"});
+            res.end(data);
+        }
+    })
+})
 
-// server.listen(port, (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("server running on port", port);
-//   }
-// });
+const port = 5000;
 
-// import http from "http";
-// import fs from "fs";
-
-// const server = http.createServer((req,res) =>{
-//     fs.readFile("index.html", (err,data) =>{
-//         if (err){
-//             res.writeHead(404)
-//             res.end("file not found");
-//         }
-//         else{
-//             res.writeHead(200, {"content-type":"text/html"});
-//             res.end(data);
-//         }
-//     })
-// })
-
-// const port = 5000;
-
-// server .listen(port, (err) => {
-//     if (err){
-//         console.log(err);
-//     }else{
-//         console.log("server running on port", port);
-//     }
-// });
-
+server .listen(port, (err) => {
+    if (err){
+        console.log(err);
+    }else{
+        console.log("server running on port", port)
+    }
+})

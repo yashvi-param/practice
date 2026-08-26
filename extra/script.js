a9624c78c8d3c3b5b13eaa1e0f603db7db6cb26a
yashvi-param <yashvirwn123@gmail.com>
2026-08-26 13:36:57 +0530

    Update JavaScript value copy example

--- extra/script.js
+++ extra/script.js
@@ -1,87 +1,11 @@
-//1. string ->textual data
+let num1 = 100;
 
-let name = "alice";
+let num2 = num1;
 
-console.log(name);
+console.log("num1 value", num1);
+console.log("num2 value", num2);
 
-// typeOf operator is used to check dataTypes of operator
+num2 = 500;
 
-console.log("type of name is ", typeof name);
-
-// using single quat
-
-name = "charlie";
-
-console.log(name);
-
-// 2. Number
-
-let num = 10;
-
-console.log("num value", num);
-
-console.log("type of num is ", typeof num);
-
-num = "10";
-console.log("type of num is ", typeof num);
-
-// 3. Boolean
-
-let answer = true;
-
-console.log("answer", answer);
-
-console.log("type of answer is ", typeof answer);
-
-answer = false;
-console.log("type of answer is ", typeof answer);
-
-// 4. undefined -> work as placeholder in memory it will occupy space in memory but value haven't assign yet
-
-let a;
-console.log("a value", a);
-
-console.log("type of a ", typeof a);
-
-let b = undefined;
-
-console.log(b);
-
-// 5. null
-
-let marks = null;
-
-console.log("marks", null);
-
-console.log("type of null", typeof marks);
-
-// 6.  bigInt
-
-let bigNumber = 900719925124740999n;
-
-console.log("big numbers", bigNumber);
-
-console.log("type of bigNumber", typeof bigNumber);
-
-// 7. symbol
-
-let id = Symbol("id");
-
-console.log("id", id);
-
-console.log("type of id", typeof id);
-
-// 8. object
-
-let person = { name: "alice" };
-
-console.log("person", person);
-
-console.log("type of person", typeof person);
-
-// let fruits = ["apple", "banana", "mango"];
-let fruits = ["apple", "banana", "mango", 1, true];
-
-console.log("fruits items are", fruits);
-
-console.log(typeof fruits);
+console.log("num1 value", num1);
+console.log("num2 value", num2);

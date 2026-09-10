39d75f4b2e3da97b28ddc1777c0096e67eacab2b
yashvi-param <yashvirwn123@gmail.com>
2026-09-10 12:56:17 +0530

    Add JavaScript basic concepts

--- backend/ch-01 Module/1.Core-module/app.js
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-
-fs.writeFileSync("new.txt", "new file created");
-
-const data = fs.readFileSync("new.txt", "utf-8");
-
-console.log("data", data);
--- backend/ch-12 quickNest/middleware/HttpError.js
+++
@@ -1,8 +0,0 @@
-class HttpError extends Error { // Declares a custom error class that inherits from the built-in Error class.
-    constructor(statusCode, message) { // Constructor accepting a numeric status code and a string message.
-        super(message); //Calls the parent Error constructor with the message, setting the error message.
-        this.statusCode = statusCode; //Assigns the status code to an instance property.
-    } //Closes the constructor.
-} // Closes the class.
-
-export default HttpError; //Exports the class as the default module export.
